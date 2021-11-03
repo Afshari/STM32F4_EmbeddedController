254897fdba48b1bd940f43589e0e7cda74ce2f78
Mohsen Afshari <afshari.mvc@gmail.com>
2021-11-03 22:18:57 +0330

    Add Math (Linear Algebra) Directory and 'add', 'sub', 'scale' functions

---
+++ Inc/Math/linalg.h
@@ -0,0 +1,16 @@
+
+
+#ifndef _LINALG_H_
+#define _LINALG_H_
+
+#include <algorithm>
+#include <array>
+#include <vector>
+
+
+using std::make_shared;
+using std::shared_ptr;
+using std::vector;
+
+
+#endif /* _LINALG_H_ */
---
+++ Src/Math/sub.cpp
@@ -0,0 +1,21 @@
+
+
+#include "linalg.h"
+
+void sub( const vector<double> &A, const vector<double> &B, shared_ptr<vector<double>> C, 
+		int row, int column ) {
+
+	/*
+	 * This uses the formula C = A - B
+	 */
+			
+	C = make_shared<vector<double>>( row * column );
+			
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++) {
+			
+				int idx = (i * column) + j;
+				C->at(idx) = A.at(idx) - B.at(idx);
+		}
+	}
+}
